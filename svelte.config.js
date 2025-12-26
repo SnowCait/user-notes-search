a8a89161db82ac0900063c9cb3cb6813f293d8e9
SnowCait <SnowCait@users.noreply.github.com>
2025-12-26 09:36:00 +0900

    SSG

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,10 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({ pages: 'docs' }),
+		paths: { base: process.argv.includes('dev') ? '' : process.env.BASE_PATH }
+	}
 };
 
 export default config;
