86cb4b4b9aa0960d620a4d6d17127d80d2e58740
SnowCait <SnowCait@users.noreply.github.com>
2025-12-26 09:49:59 +0900

    Fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import { page } from '$app/state';
-	import { locales, localizeHref } from '$lib/paraglide/runtime';
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
@@ -10,10 +8,3 @@
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
 {@render children()}
-<div style="display:none">
-	{#each locales as locale}
-		<a href={localizeHref(page.url.pathname, { locale })}>
-			{locale}
-		</a>
-	{/each}
-</div>
